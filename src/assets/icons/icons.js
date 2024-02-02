125d3f57fb476d30526d41ca9e261d8c6ebd1884
illmattep <kippermint6@gmail.com>
2024-02-02 17:57:40 +0100

    Iconography and submenu logic

---
+++ src/assets/icons/icons.js
@@ -0,0 +1,27 @@
+import ControlPanel from './ControlPanel.svg';
+import Dashboard from './Dashboard.svg';
+import discounts from './discounts.svg';
+import docs from './docs.svg';
+import help from './help.svg';
+import pagesandcms from './pagesandcms.svg';
+import users from './users.svg';
+import variables from './variables.svg';
+import settings from './settings.svg';
+import DashboardSettings from './DashboardSettings.svg';
+import UserSettings from './UserSettings.svg';
+import Globe from './Globe.svg';
+export {
+    Globe,
+    UserSettings,
+    DashboardSettings,
+    ControlPanel,
+    Dashboard,
+    discounts,
+    docs,
+    help,
+    pagesandcms,
+    settings,
+    users,
+    variables
+
+};
--- src/components/SidePanel/SidePanel.tsx
+++ src/components/SidePanel/SidePanel.tsx
@@ -1,35 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SidePanel.scss';
+import * as icons from '../../assets/icons/icons';
+import arrow from '../../assets/icons/arrow.svg'; // Import the arrow icon
+
+interface MenuItem {
+  icon: string;
+  text: string;
+  isOpen: boolean;
+  options: string[];
+}
 
 const SidePanel: React.FC = () => {
+  const [menuItems, setMenuItems] = useState<MenuItem[]>([
+    { icon: icons.ControlPanel, text: 'Control Panel', isOpen: false, options: [] },
+    { icon: icons.variables, text: 'Variables', isOpen: false, options: [] },
+    { icon: icons.settings, text: 'Settings', isOpen: false, options: ['Option 1', 'Option 2', 'Option 3'] },
+    { icon: icons.discounts, text: 'Discounts', isOpen: false, options: ['Option 1', 'Option 2', 'Option 3'] },
+    { icon: icons.pagesandcms, text: 'Pages & CMS', isOpen: false, options: ['Option 1', 'Option 2', 'Option 3'] },
+  ]);
+
+  const additionalMenuItems = [
+    { icon: icons.docs, text: 'Docs' },
+    { icon: icons.Dashboard, text: 'Dashboard' },
+    { icon: icons.help, text: 'Help' },
+  ];
+
+  const toggleDropdown = (index: number) => {
+    setMenuItems((prevMenuItems) => {
+      const updatedMenuItems = prevMenuItems.map((item, i) => {
+        if (i === index) {
+          return { ...item, isOpen: !item.isOpen };
+        } else {
+          return { ...item, isOpen: false };
+        }
+      });
+      return updatedMenuItems;
+    });
+  };
+
   return (
     <div className="side-panel">
       <div className="menu">
         <div className="title">CoralMC</div>
-        <div className="menu-item">
-          <span className="icon icon-control-panel" /> Control Panel
-        </div>
-        <div className="menu-item">
-          <span className="icon icon-variables" /> Variables
-        </div>
-        <div className="menu-item">
-          <span className="icon icon-settings" /> Settings
-        </div>
-        <div className="menu-item">
-          <span className="icon icon-discounts" /> Discounts
-        </div>
-        <div className="menu-item">
-          <span className="icon icon-pages" /> Pages & CMS
-        </div>
+        {menuItems.map((item, index) => (
+          <div className={`menu-item ${item.isOpen ? 'open' : ''}`} key={index} onClick={() => toggleDropdown(index)}>
+            <div className="button-title">
+              <img src={item.icon} alt={item.text} />
+              {item.text}
+              {item.options.length > 0 && (
+                <span className={`arrow-icon ${item.isOpen ? 'flip' : ''}`}>
+                  <img src={arrow} alt="Arrow" /> {/* Use the arrow icon */}
+                </span>
+              )}
+            </div>
+            {item.isOpen && (
+              <div className="submenu">
+                {item.options.map((option, optionIndex) => (
+                  <div className="submenu-option" key={optionIndex}>
+                    {option}
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        ))}
         <div className="separator" />
-        <div className="menu-item">Docs</div>
-        <div className="menu-item">Dashboard</div>
-        <div className="menu-item">Help</div>
+        {additionalMenuItems.map((item, index) => (
+          <div className="menu-item" key={index}>
+            <div className="button-title">
+              <img src={item.icon} alt={item.text} />
+              {item.text}
+            </div>
+          </div>
+        ))}
       </div>
       <div className="icons">
-        <img src="icon1.png" alt="Icon 1" />
-        <img src="icon2.png" alt="Icon 2" />
-        <img src="icon3.png" alt="Icon 3" />
+        <img src={icons.DashboardSettings} alt="Icon 1" />
+        <img src={icons.Globe} alt="Icon 2" />
+        <img src={icons.UserSettings} alt="Icon 3" />
       </div>
     </div>
   );
